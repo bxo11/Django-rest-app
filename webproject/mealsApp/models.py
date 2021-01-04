e9380c39fe900c9dfd4d5a5b28ad132fbc511f24
KrzysiekJa <kjmj77@gmail.com>
2021-01-04 17:05:59 +0100

    Added validation of views for connection with database

--- webproject/mealsApp/models.py
+++ webproject/mealsApp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.utils.timezone import now
+from django.core.validators import MinLengthValidator
 
 
 class Meals(models.Model):
-    name = models.CharField(max_length=100, default="")
-    category = models.CharField(max_length=100, default="lunch")
-    instruction = models.CharField(max_length=500, default="nothing")
+    name = models.CharField(max_length=100, default="", validators=[MinLengthValidator(2, "2 or more characters")])
+    category = models.CharField(max_length=100, default="lunch", validators=[MinLengthValidator(2, "2 or more characters")])
+    instruction = models.CharField(max_length=500, default="nothing", validators=[MinLengthValidator(2, "2 or more characters")])
     dateAdded = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -13,9 +14,9 @@ class Meals(models.Model):
 
 
 class Ingredients(models.Model):
-    name = models.CharField(max_length=100, default="")
+    name = models.CharField(max_length=100, default="", validators=[MinLengthValidator(2, "2 or more characters")])
     amount = models.FloatField(default=0.)
-    amountUnit = models.CharField(max_length=100, default="")
+    amountUnit = models.CharField(max_length=100, default="", validators=[MinLengthValidator(2, "2 or more characters")])
     meal = models.ManyToManyField(Meals, related_name="ingredients_list")
 
     def __str__(self):
